b971df06b84ecde42742e76d0113426bfba853ec
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2021-02-15 12:34:15 +0530

    Initial Commit

---
+++ UI Challenge/UI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ChallengeApp.swift
+//  UI Challenge
+//
+//  Created by Azhagusundaram Tamil on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
